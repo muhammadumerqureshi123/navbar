9739d6bba0dacda9fc4abfc9f233859351b328b6
muhammadumerqureshi123 <158285683+muhammadumerqureshi123@users.noreply.github.com>
2024-02-06 03:26:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function abc() {
+    document.getElementById("nav").style.display = 'block';
+    document.getElementById("nav").style.width = '250px';
+}
+function abcd() {
+    document.getElementById("nav").style.display = 'none';
+    document.getElementById("nav").style.width = '0';
+}
